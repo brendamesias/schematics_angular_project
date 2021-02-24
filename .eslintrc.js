b0b5907c7098e9edb0e41214bdf882dc2ec10dd7
brendamesias <brendamesiasvillar@gmail.com>
2021-02-24 01:22:38 -0500

    configuration lint staged

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['plugin:@angular-eslint/recommended'],
+  overrides: [
+    {
+      files: ['*.html'],
+      rules: {
+        'max-len': 'off'
+      }
+    },
+    {
+      files: ['*.component.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      plugins: ['@angular-eslint/template'],
+      processor: '@angular-eslint/template/extract-inline-html',
+    },
+    {
+      files: ["*.ts"],
+      extends: [
+        'airbnb-typescript/base',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      rules: {
+         // Custom rules
+         'import/no-unresolved': 'off',
+         'import/prefer-default-export': 'off',
+         'class-methods-use-this': 'off',
+         'lines-between-class-members': 'off',
+         "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
+         '@typescript-eslint/unbound-method': [
+           'error',
+           {
+             ignoreStatic: true,
+           }
+         ]
+      }
+    },
+  ],
+}
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('lint-staged app is running!');
+    expect(await page.getTitleText()).toEqual('lint-staged-app app is running!');
   });
 
   afterEach(async () => {
