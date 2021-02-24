bfc271f2720894f8094133357209287b21bfaeec
brendamesias <brendamesiasvillar@gmail.com>
2021-02-24 02:08:25 -0500

    prueba precommit 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,18 @@ module.exports = {
     node: true,
   },
   extends: ['plugin:@angular-eslint/recommended'],
+  rules: {
+    // ORIGINAL tslint.json -> "directive-selector": [true, "attribute", "app", "camelCase"],
+    '@angular-eslint/directive-selector': [
+      'error',
+      { type: 'attribute', prefix: 'app', style: 'camelCase' },
+    ],
+    // ORIGINAL tslint.json -> "component-selector": [true, "element", "app", "kebab-case"],
+    '@angular-eslint/component-selector': [
+      'error',
+      { type: 'element', prefix: 'app', style: 'kebab-case' },
+    ],
+  },
   overrides: [
     {
       files: ['*.html'],
